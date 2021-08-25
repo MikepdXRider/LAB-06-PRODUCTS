bc53f1095fddcf16258d65653b90ac7e30795d78
Michael Rider <mikepdxrider@gmail.com>
2021-08-25 15:22:15 -0700

    adds checkout button eventListener

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -1,9 +1,10 @@
 import { renderLineItems } from './render-line-items.js';
-import { calcOrderTotal, getCart } from '../utils.js';
+import { calcOrderTotal, clearCart, getCart } from '../utils.js';
 import { productsArray } from '../products/duckys.js';
 
 const tBodyEl = document.querySelector('#t-body');
 const tdCartTotal = document.getElementById('cart-total');
+const checkoutButton = document.getElementById('checkout-button');
 
 const cart = getCart();
 
@@ -15,3 +16,17 @@ for (let obj of cart) {
 const cartTotal = calcOrderTotal(cart, productsArray);
 
 tdCartTotal.textContent = cartTotal;
+
+    //User clicks 'Check Out' button.
+checkoutButton.addEventListener('click', () => {
+    //     - getCart() to retrieve cart array
+    const cartContents = getCart();
+    //     - stringify array so it's pretty.
+    const cartContentsString = JSON.stringify(cartContents, true, 2);
+    // 1. Alert user of cart contents
+    alert(cartContentsString);
+    // 2. Clear cart contents.
+    clearCart();
+    // 3. Take user to home. 
+    window.location = '../index.html';
+})
