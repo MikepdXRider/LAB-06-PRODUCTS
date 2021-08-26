ab2a5612dfe4395e7d01881da06f9ee4e773b703
Michael Rider <mikepdxrider@gmail.com>
2021-08-25 17:36:06 -0700

    adds drop quantity selector to products page

--- products/render-products.js
+++ products/render-products.js
@@ -6,6 +6,11 @@ export function renderProducts(obj){
     const titleDiv = document.createElement('div');
     const h3 = document.createElement('h3');
     const button = document.createElement('button');
+    const form = document.createElement('form');
+    const select = document.createElement('select');
+    const option1 = document.createElement('option');
+    const option2 = document.createElement('option');
+    const option3 = document.createElement('option');
     const img = document.createElement('img');
     const descriptionP = document.createElement('p');
     const priceDiv = document.createElement('div');
@@ -14,6 +19,9 @@ export function renderProducts(obj){
 
     h3.textContent = obj.name;
     button.textContent = '+';
+    option1.textContent = '1';
+    option2.textContent = '2';
+    option3.textContent = '3';
     img.src = obj.img;
     descriptionP.textContent = obj.description;
     categoryP.textContent = obj.category;
@@ -24,15 +32,19 @@ export function renderProducts(obj){
     h3.classList.add('product-name');
     button.classList.add('add-to-cart-button');
     button.setAttribute('id', 'add-to-cart-button');
+    select.setAttribute('id', 'user-input');
     img.setAttribute('alt', 'product-image');
     descriptionP.classList.add('description');
     priceDiv.classList.add('flex-price-category');
 
     button.addEventListener('click', () => {
-        addItemToCart(obj.id);
+        const userQtyInput = Number(select.value);
+        addItemToCart(obj.id, userQtyInput);
     });
 
-    titleDiv.append(h3, button);
+    select.append(option1, option2, option3);
+    form.append(select);
+    titleDiv.append(h3, button, form);
     priceDiv.append(categoryP, priceP);
     li.append(titleDiv, img, descriptionP, priceDiv);
     
