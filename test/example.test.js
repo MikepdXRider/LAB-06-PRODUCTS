f310ddf110c2b4db849675ec20bcda05cdfddb41
Michael Rider <mikepdxrider@gmail.com>
2021-08-25 18:25:36 -0700

    resolves CI issues

--- test/example.test.js
+++ test/example.test.js
@@ -3,7 +3,7 @@ import { renderProducts } from '../products/render-products.js';
 import { renderLineItems } from '../shopping-cart/render-line-items.js';
 import { findById } from '../utils.js';
 import { calcItemTotal, calcOrderTotal } from '../shopping-cart/shopping-cart-utils.js';
-import { getCart, setCart } from '../cart-api.js';
+import { getCart } from '../cart-api.js';
 
 
 const test = QUnit.test;
@@ -54,7 +54,7 @@ test('input 3 and 3 into calcItemTotal function, expect 9', (expect) => {
 test('input an object into renderProducts function, expect an HTML element', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = '<li class="product-list-item"><div class="title-button-container"><h3 class="product-name">product-name</h3><div><button class="add-to-cart-button" id="add-to-cart-button">+</button><form><select id=\"user-input\"><option>1</option><option>2</option><option>3</option></select></form></div></div><img src="https://placekitten.com/320/200" alt="product-image"><p class="description">Product description that is cool AF</p><div class="flex-price-category"><p>product-category</p><p>$4</p></div></li>';
+    const expected = '<li class="product-list-item"><div class="title-button-container"><h3 class="product-name">product-name</h3><div><button class="add-to-cart-button" id="add-to-cart-button">+</button><form><select id="user-input"><option>1</option><option>2</option><option>3</option></select></form></div></div><img src="https://placekitten.com/320/200" alt="product-image"><p class="description">Product description that is cool AF</p><div class="flex-price-category"><p>product-category</p><p>$4</p></div></li>';
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = renderProducts({
