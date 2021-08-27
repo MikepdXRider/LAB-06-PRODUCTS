bc831f2b1cbe2f55fc35cee63618c4ad6687f95a
Michael Rider <mikepdxrider@gmail.com>
2021-08-27 15:44:36 -0700

    adds admin page plan and files

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ export const cartLinkNodeList = document.querySelectorAll('.cart-link');
 // Check cart if empty before allowing link to go through.
 
 
-
 for (let i = 0; i < cartLinkNodeList.length; i++){
     const checkCart = checkCartStatus();
     if (checkCart === false) {
