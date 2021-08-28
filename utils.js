e824e980f2355f67913221a8b368dc630188f136
Michael Rider <mikepdxrider@gmail.com>
2021-08-27 18:13:36 -0700

    resolves console errors

--- utils.js
+++ utils.js
@@ -6,3 +6,16 @@ export function findById(id, arrayOfProducts){
         }
     }
 }
+
+import { checkCartStatus } from './cart-api.js';
+
+export const cartLinkNodeList = document.querySelectorAll('.cart-link');
+// Check cart if empty before allowing link to go through.
+
+
+for (let i = 0; i < cartLinkNodeList.length; i++){
+    const checkCart = checkCartStatus();
+    if (checkCart === false) {
+        cartLinkNodeList[i].style.visibility = 'hidden';
+    }
+}
